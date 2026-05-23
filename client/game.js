47caf172097086ace7d02b5ed19c5879a30c4911
Elay319 <183721374+Elay319@users.noreply.github.com>
2026-05-23 08:46:13 +0300

    Update game.js

--- client/game.js
+++ client/game.js
@@ -12,8 +12,7 @@ window.addEventListener('keydown', (e) => {
 // ========================================
 // MULTIPLAYER
 // ========================================
-
-const socket = io('http://10.100.102.122:3000');
+const socket = io('https://game-euks.onrender.com');
 
 const otherPlayers = {};
 const scene = new THREE.Scene();
@@ -1117,7 +1116,7 @@ async function publishCurrentGame() {
   // SEND TO SERVER
 
 await fetch(
-  'http://10.100.102.122:3000/publish',
+  'https://game-euks.onrender.com/publish',
   {
 
     method: 'POST',
@@ -1144,7 +1143,7 @@ await fetch(
 async function loadServerGames() {
 
   const response = await fetch(
-    'http://10.100.102.122:3000/games'
+    'https://game-euks.onrender.com/games'
   );
 
   const games =
